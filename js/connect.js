fd1a042b8eb81ead2acbef8f1eed1c717e25e00d
PavelPopovIPS <PavelPopovIPS@users.noreply.github.com>
2024-03-31 18:19:39 +0300

    поправил вывод сообщений

--- js/connect.js
+++ js/connect.js
@@ -1,4 +1,4 @@
-let count = 0;
+let count = 1;
 
 const responseBlock = document.querySelector('#response');
 const button = document.querySelector('#submitButton');
@@ -9,21 +9,23 @@ button.addEventListener('click', () => {
 
     if (input.value === '') {
         input.style['border-color'] = 'red';
-        responseBlock.innerHTML = 'Введите ваш ответ!';
+        printMessage('Введите ваш ответ!');
         return
     }
 
-    let output = `Попытка ответа: ${++count}. Ваш ответ: ${input.value}.`;
-
-    if (count % 5 == 0) {
-        responseBlock.innerHTML = 'Ой, что-то произошло: Код ответа: 500';
+    if (count % 4 == 0) {
+        printResponse('Ой, что-то произошло: Код ответа: 500');
         return
     }
 
-    if (count % 3 == 0) {
-        responseBlock.innerHTML = 'Возможно, вы сделали, что-то не то. Код ответа: 400';
-        return
-    }
-
-    responseBlock.innerHTML = output;
+    printResponse(`Попытка ответа: ${count}. <br> Ваш ответ: ${input.value}.`);
 });
+
+function printResponse(msg) {
+    printMessage(msg)
+    ++count
+}
+
+function printMessage(msg) {
+    responseBlock.innerHTML = msg
+}
